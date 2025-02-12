9b09dbb473b5a392c4b3d72c771530bb7033c18a
YassirJr <yassir.junior.03@gmail.com>
2025-02-13 00:06:00 +0100

    use a global use service actions

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,22 +1,20 @@
+"use server"
+
 import {signIn} from "@/auth";
-import {signIn as clt} from "next-auth/react";
 import {SignInResponse} from "next-auth/react";
 
-export const authService = {
-    credentials: async <T>(credentials: T): Promise<SignInResponse | undefined> => {
-        return await clt("credentials", {
-            redirect: true,
-            redirectTo: "/",
-            ...credentials
-        })
-    },
-    oauth: {
-        github: async (): Promise<void> => {
-            "use server"
-            await signIn("github", {
-                redirect: true,
-                redirectTo: "/"
-            })
-        },
-    }
+ export async function credentialsAuth<T>(values: T): Promise<SignInResponse> {
+    return await signIn("credentials", {
+        redirect: true,
+        redirectTo: "/",
+        ...values,
+    });
 }
+
+ export async function githubAuth(): Promise<SignInResponse> {
+    return await signIn("github", {
+        redirect: true,
+        redirectTo: "/",
+    });
+}
+
